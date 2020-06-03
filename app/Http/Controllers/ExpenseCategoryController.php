76221eb3c8f88d4185c64f319d95b675ec0bff70
rahulhaque <rahulhaque07@gmail.com>
2020-06-03 15:53:27 +0600

    Expense category partial search added

--- app/Http/Controllers/ExpenseCategoryController.php
+++ app/Http/Controllers/ExpenseCategoryController.php
@@ -40,6 +40,7 @@ class ExpenseCategoryController extends Controller
         if ($request->get('search_col') || $request->get('search_by')) {
             $query->where(
                 $request->get('search_col') ? $request->get('search_col') : 'id',
+                'like',
                 $request->get('search_by') ? $request->get('search_by') : ''
             );
         }
